3a27a6cd321db72bc65e7c146a350294b7f237e7
Scherpschutter <konrdlisthrop@gmail.com>
2024-07-03 22:05:14 -0500

    psycopg2

--- MentorM/settings.py
+++ MentorM/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 AUTH_USER_MODEL = "security.User"
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)# carpeta fisica de archivos estaticos
